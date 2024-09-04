c49ae7ce8e7f861ad07c95b304696e5d0fb4c28a
david <dalejandro1111@gmail.com>
2024-09-04 12:11:14 -0600

    se agrega el servicio para stock

---
+++ src/main/java/com/dw/ventas/models/StockUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.dw.ventas.models;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class StockUpdateRequest {
+
+    private Integer existencia;
+}
